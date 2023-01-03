974746e32a7d43f498e1589a43e359105bc13711
kucsssss <117690564+kucsssss@users.noreply.github.com>
2023-01-03 13:33:53 +0100

    Made some minor changes in the registartion service, so it can now register new users.

--- src/main/java/com/example/reglog/registration/RegistrationService.java
+++ src/main/java/com/example/reglog/registration/RegistrationService.java
@@ -1,10 +1,32 @@
 package com.example.reglog.registration;
 
+import com.example.reglog.appuser.AppUser;
+import com.example.reglog.appuser.AppUserRole;
+import com.example.reglog.appuser.AppUserService;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class RegistrationService {
+
+    private static AppUserService appUserService;
+    private static EmailValidator emailValidator;
     public static String register(RegistrationRequest request) {
-        return "it works";
+        boolean isValidEmail = emailValidator
+                .test(request.getEmail());
+        if (!isValidEmail) {
+            throw new IllegalStateException("email not valid!");
+        }
+
+        return appUserService.signUpUser(
+                new AppUser(
+                        request.getFirstName(),
+                        request.getLastName(),
+                        request.getEmail(),
+                        request.getPassword(),
+                        AppUserRole.USER
+                )
+        );
     }
 }
