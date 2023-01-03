e17737499ac25ba712ea440e84acfd9f48187eb4
kucsssss <117690564+kucsssss@users.noreply.github.com>
2023-01-03 14:15:27 +0100

    Typo fix

--- src/main/java/com/example/reglog/registration/EmailValidator.java
+++ src/main/java/com/example/reglog/registration/EmailValidator.java
@@ -5,7 +5,9 @@ import org.springframework.stereotype.Service;
 import java.util.function.Predicate;
 
 @Service
-public class EmailValidate implements Predicate <String> {
+public class EmailValidator implements Predicate <String> {
     @Override
-    public boolean test(String s)
+    public boolean test(String s){
+        return true;
+    }
 }
