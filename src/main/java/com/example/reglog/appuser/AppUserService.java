44c23c7f029e40747af3c50915b214c671e74739
kucsssss <117690564+kucsssss@users.noreply.github.com>
2023-01-03 13:36:38 +0100

    Made a statement if the given email was already taken.

--- src/main/java/com/example/reglog/appuser/AppUserService.java
+++ src/main/java/com/example/reglog/appuser/AppUserService.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 
@@ -15,6 +16,7 @@ public class AppUserService implements UserDetailsService {
     private final static String USER_NOT_FOUND_msg =
             "user with email %s not found";
     private final AppUserRepository appUserRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
 
     @Override
@@ -22,5 +24,17 @@ public class AppUserService implements UserDetailsService {
             throws UsernameNotFoundException {
         return appUserRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException(String.format(USER_NOT_FOUND_msg, email)));
+    }
+
+    public String signUpUser(AppUser appUser){
+        boolean userExists = appUserRepository
+                .findByEmail(appUser.getEmail())
+                .isPresent();
+        if(userExists){
+            throw new IllegalStateException("email already taken");
+        }
+
+        bCryptPasswordEncoder.encode(appUser.getPassword());
+        return "";
     }
 }
